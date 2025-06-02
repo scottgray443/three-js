5e7f05cf96aba2782443a407ca554af19b16fd47
scottgray443 <138357328+scottgray443@users.noreply.github.com>
2025-06-02 18:10:55 +1000

    Update script.js

--- cg-shader-distortion-img-hover-effect 4/script.js
+++ cg-shader-distortion-img-hover-effect 4/script.js
@@ -24,8 +24,33 @@ async function loadShader(url) {
 }
 
 // Load vertex and fragment shaders
-const vertexShader = await loadShader("./shaders/vertexShader.glsl");
-const fragmentShader = await loadShader("./shaders/fragmentShader.glsl");
+const vertexShader = `
+attribute vec2 uv;
+attribute vec2 position;
+varying vec2 vUv;
+void main() {
+  vUv = uv;
+  gl_Position = vec4(position, 0, 1);
+}
+`;
+
+const fragmentShader = `
+precision highp float;
+precision highp int;
+uniform sampler2D tWater;
+uniform sampler2D tFlow;
+uniform float uTime;
+varying vec2 vUv;
+uniform vec4 res;
+
+void main() {
+  vec3 flow = texture2D(tFlow, vUv).rgb;
+  vec2 myUV = vUv - flow.xy * 0.3; 
+  vec3 tex = texture2D(tWater, myUV).rgb;
+
+  gl_FragColor = vec4(tex, 1.0);
+}
+`;
 
 // Define image resolution
 const _size = [2048, 1638];
